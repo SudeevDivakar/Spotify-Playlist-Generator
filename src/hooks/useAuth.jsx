5281165592037c2831fd23391e6c2ee7a0c7447a
Sudeev Divakar <sudeev.divakar@gmail.com>
2024-07-27 23:10:05 +0530

    Add a Refresh Token Hook

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import queryString from "query-string";
+import Cookies from "js-cookie";
+
+export default function useAuth() {
+  const [accessToken, setAccessToken] = useState(Cookies.get("access_token"));
+  const navigate = useNavigate();
+
+  async function refreshAccessToken() {
+    const refreshToken = Cookies.get("refresh_token");
+    console.log(refreshToken);
+    if (!refreshToken) {
+      navigate("/");
+    } else {
+      const encodedCredentials = btoa(
+        `${import.meta.env.VITE_CLIENT_ID}:${
+          import.meta.env.VITE_CLIENT_SECRET
+        }`
+      );
+
+      const response = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        queryString.stringify({
+          grant_type: "refresh_token",
+          refresh_token: refreshToken,
+        }),
+        {
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${encodedCredentials}`,
+          },
+        }
+      );
+
+      const { access_token } = response.data;
+
+      const inOneHour = new Date(new Date().getTime() + 60 * 60 * 1000);
+      Cookies.set("access_token", access_token, {
+        expires: inOneHour,
+      });
+      setAccessToken(access_token);
+    }
+  }
+
+  useEffect(() => {
+    if (!accessToken) {
+      refreshAccessToken();
+    }
+  }, [accessToken]);
+
+  return [accessToken, refreshAccessToken];
+}
